e38d6f002aa2ca0abfe4e301328b8b40d4394477
Ujjwal Kumar <katanadeveloper01@gmail.com>
2021-01-08 23:32:39 +0530

    basic req

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 	fmt.Println("hey")
+	r.Run(":8080")
 }
