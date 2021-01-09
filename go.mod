13cf8bbc7b35f58641d0d88a76a5c7a0a39bb1c6
Ujjwal Kumar <katanadeveloper01@gmail.com>
2021-01-09 15:47:19 +0530

    connected to database and buid a model

--- go.mod
+++ go.mod
@@ -16,6 +16,6 @@ require (
 	golang.org/x/sys v0.0.0-20210108172913-0df2131ae363 // indirect
 	google.golang.org/protobuf v1.25.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gorm.io/driver/sqlite v1.1.4 // indirect
-	gorm.io/gorm v1.20.10 // indirect
+	gorm.io/driver/sqlite v1.1.4
+	gorm.io/gorm v1.20.10
 )
--- main.go
+++ main.go
@@ -2,9 +2,26 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
+// Item Model
+type Item struct {
+	gorm.Model
+	Name        string
+	Description string
+	Upvotes     int
+}
+
 func main() {
+	db, error := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if error != nil {
+		panic("Connection to database can't be established")
+	}
+
+	db.AutoMigrate(&Item{})
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "pong"})
